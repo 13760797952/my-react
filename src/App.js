9e605d227f9bd6cc3dca6bcd43baf4b07c741d61
lin <linbaoyi2015@163.com>
2019-03-28 14:22:03 +0800

    简单react的使用

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
+class App extends React.Component{
+  render (){
+    const boss = "李云龙"
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>独立团,团长{boss}</h1>
+        <Camp boss="张大喵"></Camp>
+        <Squadron boss="孙德胜"></Squadron>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function Squadron(props){
+  return <h2>骑兵连连长{props.boss},冲啊</h2>
+}
+
+class Camp extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      solders:['虎子','柱子','王根生']
+    }
+    // this.addSolder = this.addSolder.bind(this)
+  }
+  addSolder = ()=>{
+    this.setState({
+      solders:[...this.state.solders,'新兵'+Math.ceil(Math.random()*100)]
+    })
+  }
+  render(){
+    return (
+      <div>
+        <h2>一营营长,{this.props.boss}</h2>
+        <button onClick={this.addSolder}>新兵入伍</button>
+        <ul>
+          {this.state.solders.map(v=>{
+            return <li key={v}>{v}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
